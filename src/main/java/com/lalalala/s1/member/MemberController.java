003a0603109afe72de2dcd2b174713345cb57299
kodahee <daheeko0604@naver.com>
2021-03-18 17:19:39 +0900

    Update MemberController.java

--- src/main/java/com/lalalala/s1/member/MemberController.java
+++ src/main/java/com/lalalala/s1/member/MemberController.java
@@ -22,13 +22,7 @@ public class MemberController {
 	
 	// memberJoin2
 	@RequestMapping(value = "/member/memberJoin", method = RequestMethod.POST)
-	public void memberJoin2(String id, String pw, String name, String phone, String email) throws Exception {
-		MemberDTO memberDTO = new MemberDTO();
-		memberDTO.setId(id);
-		memberDTO.setPw(pw);
-		memberDTO.setName(name);
-		memberDTO.setPhone(phone);
-		memberDTO.setEmail(email);
+	public void memberJoin2(MemberDTO memberDTO) throws Exception {
 		
 		int result = memberService.memberJoin(memberDTO);
 		
